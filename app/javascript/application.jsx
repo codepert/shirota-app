1663dc5ee82cf78b0193d6f1bf8896d8850b4f06
tranguixdev <trang.uixdev@gmail.com>
2024-02-29 01:40:16 +0900

    add: react component.

---
+++ app/javascript/application.jsx
@@ -0,0 +1,15 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+// import Layout from "./layouts/Layout";
+// import Home from "./pages";
+import Page from "./page";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ app/javascript/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+  // const { language } = useLanguage();
+  return (
+    <>
+      <h1>React component</h1>
+    </>
+  );
+};
+
+export default Page;

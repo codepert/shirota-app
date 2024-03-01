5dd4b66058118341e68faf8554be89fe6ebe8689
tranguixdev <trang.uixdev@gmail.com>
2024-03-01 18:45:03 +0900

    Add initial route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,32 @@
 Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
   root 'components#index'
+
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
   }
+  # controllers: {
+  #   sessions: 'users/sessions',
+  #   registrations: 'users/registrations'
+  # }
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, format: "json" do
+    namespace :v1 do
+      resources :authorities
+      resources :user_authorities
+      resources :authority_pages
+      resources :warehouses
+      resources :warehouse_fees
+      resources :shippers
+      resources :products
+      resources :stocks
+      resources :stock_inouts
+      resources :receive_payments
+      resources :bills
+      resources :bill_amounts
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
